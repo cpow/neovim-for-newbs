60e0289fec461cf699b69fc499329b792952769b
Christopher Power <chris.power@underdogfantasy.com>
2024-01-06 17:04:41 -0500

    debugging baby

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,31 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"leoluz/nvim-dap-go",
+		"rcarriga/nvim-dap-ui",
+	},
+	config = function()
+    require("dapui").setup()
+		require("dap-go").setup()
+
+		local dap, dapui = require("dap"), require("dapui")
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
+		vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
+		vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
+		vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+	end,
+}
