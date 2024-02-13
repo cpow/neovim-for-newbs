b5710410afbee486f86c5854faf2988d42300c9d
Christopher Power <chris.power@underdogfantasy.com>
2024-02-13 10:25:44 -0500

    add fugitive and gitsigns

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,7 +5,7 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme "catppuccin"
+      vim.cmd.colorscheme "catppuccin-mocha"
     end
   }
 }
