e77feed24986d90b41f10a10d3b52fe0d66f9e3f
Ashish Verma <19263858+vermacodes@users.noreply.github.com>
2024-03-14 20:56:26 +0000

    eslint_d has been removed from builtins, install it from extras.

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,5 +1,8 @@
 return {
 	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
 	config = function()
 		local null_ls = require("null-ls")
 		null_ls.setup({
@@ -7,9 +10,9 @@ return {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.diagnostics.erb_lint,
-				null_ls.builtins.diagnostics.eslint_d,
 				null_ls.builtins.diagnostics.rubocop,
 				null_ls.builtins.formatting.rubocop,
+				require("null_ls.diagnostics.eslint_d"),
 			},
 		})
 
